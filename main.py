94463154e1b1b380bbc9ab3d4f767a539b597f86
ThatGuyIKnow <oliverhsj@hotmail.dk>
2018-07-25 18:36:27 +0200

    Beutify

--- main.py
+++ main.py
@@ -15,14 +15,28 @@ class EscapeRoom(App):
         layout2 = BoxLayout(orientation='vertical')
         layout.add_widget(layout2)
 
-        countdown = Label(text='00:00', bold=True, font_size=50)
-        img = Image(source='WHO.jpg', allow_stretch=True)
+        countdown = Label(text='00:00',
+                          bold=True,
+                          font_size=50)
+
+        img = Image(source='WHO.jpg',
+                    allow_stretch=True)
+
+        passwordBox = FloatLayout()
+
         layout.add_widget(countdown)
         layout2.add_widget(img)
-        passwordBox = FloatLayout()
         layout2.add_widget(passwordBox)
-        textinput = TextInput(hint_text='Password', multiline=False, size_hint=(.7, .1), pos_hint={'x': .15, 'y': .5})
-        btn = Button(text='CHECK', size_hint=(.4, .1), pos_hint={'x': .3, 'y': .3})
+
+        textinput = TextInput(hint_text='Password',
+                              multiline=False,
+                              size_hint=(.7, .1),
+                              pos_hint={'x': .15, 'y': .5})
+
+        btn = Button(text='CHECK',
+                     size_hint=(.4, .1),
+                     pos_hint={'x': .3, 'y': .3})
+        
         passwordBox.add_widget(textinput)
         passwordBox.add_widget(btn)
 

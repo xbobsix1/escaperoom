704e6b1c3ecb3e71bccf07ed38e95c986e0bb0e3
ThatGuyIKnow <oliverhsj@hotmail.dk>
2018-07-25 19:53:19 +0200

    CountdownTimer

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import math
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.animation import Animation
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty, NumericProperty
 from kivy.core.window import Window
+
+class CountdownTimer(Label):
+    a = NumericProperty(3600)
+
+    def start(self):
+        Animation.cancel_all(self)
+        self.anim = Animation(a=0, duration=self.a)
+
+        def finish_callback(animation, incr_crude_clock):
+            incr_crude_clock.text = "00."
+
+        self.anim.bind(on_complete=finish_callback)
+        self.anim.start(self)
+
+    def on_a(self, instance, value):
+        self.text = str(math.floor(value/60)) + ':' + str(round(value%60, 1))
 
 
 class EscapeRoom(App):
@@ -15,9 +34,8 @@ class EscapeRoom(App):
         layout2 = BoxLayout(orientation='vertical')
         layout.add_widget(layout2)
 
-        countdown = Label(text='00:00',
-                          bold=True,
-                          font_size=50)
+        countdown = CountdownTimer()
+        countdown.start()
 
         img = Image(source='WHO.jpg',
                     allow_stretch=True)
@@ -36,12 +54,11 @@ class EscapeRoom(App):
         btn = Button(text='CHECK',
                      size_hint=(.4, .1),
                      pos_hint={'x': .3, 'y': .3})
-        
+
         passwordBox.add_widget(textinput)
         passwordBox.add_widget(btn)
 
         return layout
-
 
 if __name__ == "__main__":
     EscapeRoom().run()

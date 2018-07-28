a3d9e714958e49bda6f7cf8ebc6181707062adea
ThatGuyIKnow <oliverhsj@hotmail.dk>
2018-07-28 02:15:51 +0200

    ScreenManager implemented
    BIG UPDATE
    https://kivy.org/docs/api-kivy.uix.screenmanager.html
    Screen Manager has been implemented, and the first logo and video stages have been implemented.

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.image import Image
-from escaperoom.widgets import CountdownTimer
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.video import Video
+from escaperoom.widgets import CountdownTimer
+from escaperoom.widgets import KeyboardListener
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+import escaperoom.custom_lib
 from kivy.core.window import Window
 
 
-class EscapeRoom(BoxLayout):
+class IntroLogo(Screen):
     def __init__(self, **kwargs):
-        super(EscapeRoom, self).__init__(**kwargs)
-        # Window.fullscreen = True
-        self.orientation = 'horizontal'
-        self.layout2 = BoxLayout(orientation='vertical')
-        self.add_widget(self.layout2)
+        super(IntroLogo, self).__init__(**kwargs)
 
-        self.countdown = CountdownTimer(font_size=50,
-                                        bold=True)
-        self.countdown.start()
+        # Add starting Logo
+        self.logo = Image(source='WHO.jpg')
+        self.add_widget(self.logo)
+
+        ## Specific function that unbinds a KeyboardsListener and then switches to next screen
+        def switch():
+            self.keyboardListener.unbind()
+            self.remove_widget(self.keyboardListener)
+            sm.switch_to(VideoScreen())
+
+        # Add custom KeyBoardListener that triggers the switch-function on enter-key
+        self.keyboardListener = KeyboardListener('enter', switch)
+        self.add_widget(self.keyboardListener)
+
+
+class VideoScreen(Screen):
+    def __init__(self, **kwargs):
+        super(VideoScreen, self).__init__(**kwargs)
+        self.video = Video(source='test.mkv', state='stop')
+
+        # Specific function that switches to EscapeRoom screen
+        def switch(self, kwargs):
+            sm.switch_to(PasswordScreen())
+
+        # Bind a callback to eos event. Eos is when the video ends
+        self.video.bind(eos=switch)
+        self.add_widget(self.video)
+
+    # When the screen enters view, play the video
+    def on_enter(self, *args):
+        self.video.state = 'play'
+
+
+class PasswordScreen(Screen):
+    def __init__(self, **kwargs):
+        super(PasswordScreen, self).__init__(**kwargs)
+        this = self
+        self.layout = BoxLayout(orientation='horizontal')
+        self.add_widget(self.layout)
+        self.layout2 = BoxLayout(orientation='vertical')
+        self.layout.add_widget(self.layout2)
+
+        # Start the global timer and add a Label to EscapeRoom > layout
+        timer.start()
+        self.countdown = Label(text=timer.text, font_size=50, bold=True)
+        self.layout.add_widget(self.countdown)
+
+        # Set the countdown-label to be equal to our global timer
+        def update(dt):
+            self.countdown.text = timer.text
+
+        # Update our ocuntdown-text every
+        Clock.schedule_interval(update, 1.0 / 15.0)
 
         self.img = Image(source='WHO.jpg',
                          allow_stretch=True)
 
         self.passwordBox = FloatLayout()
 
-        self.add_widget(self.countdown)
         self.layout2.add_widget(self.img)
         self.layout2.add_widget(self.passwordBox)
 
@@ -34,28 +85,108 @@ class EscapeRoom(BoxLayout):
                                    size_hint=(.7, .1),
                                    pos_hint={'x': .15, 'y': .5})
 
+        def check_password(self):
+            if sm.get_screen(sm.current).textinput.text == '123':
+                switch()
+
         self.btn = Button(text='CHECK',
                           size_hint=(.4, .1),
-                          pos_hint={'x': .3, 'y': .3})
+                          pos_hint={'x': .3, 'y': .3},
+                          on_press=check_password)
 
         self.passwordBox.add_widget(self.textinput)
         self.passwordBox.add_widget(self.btn)
 
+        def switch():
+            sm.switch_to(RapportScreen())
+
+
+class RapportScreen(Screen):
+    def __init__(self, **kwargs):
+        super(RapportScreen, self).__init__(**kwargs)
+        self.layout = BoxLayout(orientation='horizontal')
+        self.add_widget(self.layout)
+        self.layout2 = BoxLayout(orientation='vertical', padding=[20,20])
+        self.layout.add_widget(self.layout2)
+
+        # Start the global timer and add a Label to EscapeRoom > layout
+        timer.start()
+        self.countdown = Label(text=timer.text, font_size=50, bold=True)
+        self.layout.add_widget(self.countdown)
+
+        # Set the countdown-label to be equal to our global timer
+        def update(dt):
+            self.countdown.text = timer.text
+
+        # Update our ocuntdown-text every
+        Clock.schedule_interval(update, 1.0 / 15.0)
+
+        # Adding the layout of the entire left side
+        self.agent = Label(text='DISEAS AGENT', font_size=20)
+        self.agent_input = TextInput(multiline=False)
+
+        self.zero_name = Label(text='PATIENT ZERO NAME', font_size=20)
+        self.zero_name_input = TextInput(multiline=False)
+
+        self.structure = Label(text='MOLECULE STRUCTURE', font_size=20)
+
+        self.double_bond = Label(text='DOUBLE BONDS', font_size=20)
+        self.double_bond_input = TextInput(multiline=False)
+
+        self.triple_bond = Label(text='TRIPLE BONDS', font_size=20)
+        self.triple_bond_input = TextInput(multiline=False)
+
+        self.check_button = Button(text='GENERATE', font_size=40)
+
+        self.layout2.add_widget(self.agent)
+        self.layout2.add_widget(self.agent_input)
+        self.layout2.add_widget(self.zero_name)
+        self.layout2.add_widget(self.zero_name_input)
+        self.layout2.add_widget(self.structure)
+        self.layout2.add_widget(self.double_bond)
+        self.layout2.add_widget(self.double_bond_input)
+        self.layout2.add_widget(self.triple_bond)
+        self.layout2.add_widget(self.triple_bond_input)
+        self.layout2.add_widget(self.check_button)
+
+        def switch():
+            # sm.switch_to(xxxxx)
+            pass
+
+        def check_info():
+
+            correct_answers = 0
+
+            if self.agent_input.text.lower() == '123':
+                correct_answers += 1
+
+            if self.zero_name_input.text == '123':
+                correct_answers += 1
+
+            if '123' in self.double_bond_input.text:
+                correct_answers += 1
+
+            if self.triple_bond_input.text == '123':
+                correct_answers += 1
+
+            # (WIP) REPLACE THE LEFT LAYOUT FOR 10 SECONDS WHILE ANIMATING A PROGRESS BAR. IF AFTER THE FACT THERE
+                # ARE 4 CORRECT ANSWERS, THEN PROCCED TO VICTORY SCREEN. IF THERE IS AN INCORRECT ANSWER,
+                # MARK THE INCORRECT INFORMATION AS RED AND BOLD. REMOVE ON FOCUS
+
+# Window.fullscreen = True
+
+# Add a global timer, that keeps track of the countdown between Screens
+global timer
+timer = CountdownTimer()
+
+# Add a screen manager and a starting screen
+sm = ScreenManager()
+sm.add_widget(IntroLogo())
+
 
 class Main(App):
-    global stage
-    stage = 2
-
     def build(self):
-        def switch(stage):
-            switcher = {
-                0: "zero",
-                1: "one",
-                2: EscapeRoom(),
-            }
-            return switcher.get(stage, "0")
-
-        return switch(stage)
+        return sm
 
 
 if __name__ == "__main__":
--- widgets.py
+++ widgets.py
@@ -1,11 +1,39 @@
-from kivy.properties import NumericProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.animation import Animation
 from kivy.uix.label import Label
+from kivy.uix.video import Video
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
 import math
 
-class CountdownTimer(Label):
-    a = NumericProperty(3600)
 
+class KeyboardListener(Widget):
+    def __init__(self, key, callback, **args):
+        super(KeyboardListener, self).__init__()
+        self.key = key
+        self.callback = callback
+        self.args = args
+        self.activated = True
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if self.activated and keycode[1] == 'enter':
+            return self.callback(**self.args)
+
+        return True
+
+    def unbind(self, **kwargs):
+        self.activated = False
+
+
+class CountdownTimer(Widget):
+    a = NumericProperty(3600)
+    text = StringProperty('0')
     def start(self):
         Animation.cancel_all(self)
         self.anim = Animation(a=0, duration=self.a)
@@ -20,4 +48,7 @@ class CountdownTimer(Label):
         self.anim.stop(self)
 
     def on_a(self, instance, value):
-        self.text = str(math.floor(value/60)) + ':' + str(round(value%60, 1))
+        seconds = str(round(value % 60, 2))
+        if len(seconds) < 5:
+            seconds += '0'
+        self.text = str(math.floor(value / 60)) + ':' + seconds
